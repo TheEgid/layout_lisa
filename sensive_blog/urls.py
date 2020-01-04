31cc130e27f5d38e44af088668608fd37b3e8373
TheEgid <egid.f1@gmail.com>
2020-01-04 23:01:31 +0300

    Add index page layout

---
+++ sensive_blog/urls.py
@@ -0,0 +1,18 @@
+from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('post/<slug:slug>', views.post_detail, name='post_detail'),
+    path('contact/', views.contact, name='contact'),
+    path('', views.index, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
